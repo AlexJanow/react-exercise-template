5eabec6c466df14079258b2fa1924b0815651d66
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-19 15:18:06 +0200

    add exercise

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
 
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
+
+    return () => {
+      window.removeEventListener("resize");
+    };
+  }, []);
+
   return (
     <div className="App">
       <p>Window Width: {windowWidth} </p>
@@ -13,3 +23,29 @@ function App() {
 }
 
 export default App;
+
+// # Exercise 2: Window Resize
+
+// Make an effect that will be executed only on the first render of the `App`
+// component. The effect should:
+
+// - Setup an event listener for the `window` object for the `resize` event. When
+//   handling the event, set the `windowWidth` state to the new `window.innerWidth`.
+// - Make sure you return a cleanup function inside your effect that removes the
+//   event listener for the `resize` event from the `window`
+
+// ## Tips
+
+// The `window` object is accessible in every browser. It's the so called `global` object of the browser.
+
+// You can read about the global `global` object here: https://javascript.info/global-object
+
+// The `window` object also has the method `addEventListener` like HTMLElements. It has the same format:
+
+// `window.addEventListener('event-name' handlerFunction)`
+
+// The same way we can add event listeners, we can remove them, using:
+
+// `window.removeEventListener('event-name', handlerFunction)`
+
+// What is important is that the `handlerFunction` is the same function. For that reason, we have to define the function before and not use an inline anonymous function as we are used to.
