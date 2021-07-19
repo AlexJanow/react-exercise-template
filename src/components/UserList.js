d40ae72cea5f4e53a8046771607b692afd4ab6c0
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-19 21:49:55 +0200

    add input and onchange of value amount of characters

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,27 +5,55 @@ function UserList() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [amount, setAmount] = useState("10");
 
   useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
-    const url =
-      "https://randomuser.me/api/?inc=email,gender,name,picture&results=10";
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setCharacters(data.results);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setIsError(true);
-      });
+    const temp = localStorage.getItem("characters");
+    const loadedChars = JSON.parse(temp);
+    if (loadedChars) {
+      setCharacters(loadedChars);
+    }
   }, []);
+
+  useEffect(() => {
+    const temp = JSON.stringify(characters);
+    localStorage.setItem("characters", temp);
+  }, [characters]);
+
+  // function maxResults() {
+  //   const amountOfPeople = maxResults.value;
+  //   return amountOfPeople;
+  // }
+  useEffect(
+    (amountOfPeople) => {
+      setIsLoading(true);
+      setIsError(false);
+      const url = `https://randomuser.me/api/?inc=email,gender,name,picture&results=${amount}`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setCharacters(data.results);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setIsError(true);
+        });
+    },
+    [amount]
+  );
 
   return (
     <div>
+      <div>
+        <label for="maxResults">Max. results:</label>
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+          name="maxResults"
+        />
+      </div>
       <h2>{isLoading === false ? "Search Result:" : "Loading"}</h2>
       <ul>
         {isError ? "Big Error" : !isLoading && <User characters={characters} />}
